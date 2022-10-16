c1dfe5efc0beac740aabd09766d62c0d7dd8687d
mkehoe <mkehoe@extron.com>
2022-10-15 19:20:00 -0700

    Android Image source button visual state issue

---
+++ AndriodButtonVisualStateIssue1/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace AndriodButtonVisualStateIssue1;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        if (button.IsEnabled)
+        {
+            button.Text = $"Button State = Disabled";
+            button.IsEnabled = !button.IsEnabled;
+        }
+        else
+        {
+            button.Text = $"Button State = Enabled";
+            button.IsEnabled = !button.IsEnabled;
+        }
+
+    }
+}
+
